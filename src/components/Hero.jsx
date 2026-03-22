a0211e8c63d1247cbc7c0e18dac776102ffd6543
dahirfarhia44 <dahirfarhia44@gmail.com>
2026-03-22 15:36:49 +0300

    finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
 
         {/* Example floating circles */}
       <div className={styles.circle} style={{ width: '50px', height: '50px', top: '10%', left: '20%' }}><h1 className={styles.fan}>G</h1></div>
-      <div className={styles.circle} style={{ width: '30px', height: '30px', top: '50%', left: '70%' }}><h1 className={styles.fano}>O</h1></div>
+      <div className={styles.circle} style={{ width: '30px', height: '30px', top: '50%', left: '70%' }}><h1 className={styles.fan}>O</h1></div>
       <div className={styles.circle} style={{ width: '40px', height: '40px', top: '70%', left: '30%' }}><h1 className={styles.fan}>R</h1></div>
       <div className={styles.circle} style={{ width: '60px', height: '60px', top: '70%', left: '85%' }}><h1 className={styles.fan}>W</h1></div>
       
